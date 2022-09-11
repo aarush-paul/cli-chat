6e94cf0834bae3588b0bc4f36f03c0ecde587bc9
Aarush Paul <76874133+aarush-paul@users.noreply.github.com>
2022-09-11 10:58:46 +0530

    bug fixes

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const ngrok = require('ngrok');
     console.log("Your chat url is:  " + url + ". Send to you friends and ask them to paste this link in the console after running npm join-a-room command");
 })();
 console.log(" ");
-console.log("=====CHAT SERVER LOGS=====");
+console.log("=====CHAT SERVER LOGS(Press CTRL+Q to destroy room)=====");
 
     io.on("connection", (socket) => {
 	    console.log("New Connection: " + socket.id);
@@ -23,6 +23,27 @@ console.log("=====CHAT SERVER LOGS=====");
             socket.broadcast.emit("message", `${users[socket.id]} > ${text}`);
         });
     });
+    var stdin = process.stdin;
 
+    // without this, we would only get streams once enter is pressed
+    stdin.setRawMode( true );
+    
+    // resume stdin in the parent process (node app won't quit all by itself
+    // unless an error or process.exit() happens)
+    stdin.resume();
+    
+    // i don't want binary, do you?
+    stdin.setEncoding( 'utf8' );
+    
+    // on any data into stdin
+    stdin.on( 'data', function( key ){
+      // ctrl-c ( end of text )
+      if ( key === '\u0011' ) {
+        (async function() {await ngrok.disconnect(url);}());
+        process.exit();
+      }
+      // write the key to stdout all normal like
+      process.stdout.write( key );
+    });
 
 io.listen(PORT);
