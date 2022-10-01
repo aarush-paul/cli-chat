36d29d37cf8635d153a31a79c35c2d9223b25278
Aarush Paul <76874133+aarush-paul@users.noreply.github.com>
2022-10-01 13:06:21 +0000

    making cli chat look beautiful

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 const io = require('socket.io-client');
 const prompt = require('prompt-sync')();
+const chalk = require("chalk");
+console.clear();
 
+console.log(chalk.bold.blue("         CCCCCCCCCCCCCl llllll    iiii              CCCCCCCCCCCCC  hhhhhh                                         tttt          "));
+console.log(chalk.bold.blue("      CCC::::::::::::Cl :::::l   i::::i          CCC::::::::::::C  h::::h                                      ttt:::t          "));
+console.log(chalk.bold.blue("    CC:::::::::::::::Cl :::::l    iiii         CC:::::::::::::::C  h::::h                                      t:::::t          "));
+console.log(chalk.bold.blue("   C:::::CCCCCCCC::::Cl :::::l                C:::::CCCCCCCC::::C  h::::h                                      t:::::t          "));
+console.log(chalk.bold.blue("  C:::::C       CCCCCC  l::::l  iiiiiii      C:::::C       CCCCCC  h::::h hhhhh          aaaaaaaaaaaaa   ttttttt:::::ttttttt    "));
+console.log(chalk.bold.blue(" C:::::C                l::::l  i:::::i     C:::::C                h::::hh:::::hhh       a::::::::::::a  t:::::::::::::::::t    "));
+console.log(chalk.bold.blue(" C:::::C                l::::l   i::::i     C:::::C                h::::::::::::::hh     aaaaaaaaa:::::a t:::::::::::::::::t    "));
+console.log(chalk.bold.blue(" C:::::C                l::::l   i::::i     C:::::C                h:::::::hhh::::::h             a::::a tttttt:::::::tttttt    "));
+console.log(chalk.bold.blue(" C:::::C                l::::l   i::::i     C:::::C                h::::::h   h::::::h     aaaaaaa:::::a       t:::::t          "));
+console.log(chalk.bold.blue(" C:::::C                l::::l   i::::i     C:::::C                h:::::h     h:::::h   aa::::::::::::a       t:::::t          "));
+console.log(chalk.bold.blue(" C:::::C                l::::l   i::::i     C:::::C                h:::::h     h:::::h  a::::aaaa::::::a       t:::::t          "));
+console.log(chalk.bold.blue("  C:::::C       CCCCCC  l::::l   i::::i      C:::::C       CCCCCC  h:::::h     h:::::h a::::a    a:::::a       t:::::t    tttttt"));
+console.log(chalk.bold.blue("   C:::::CCCCCCCC::::Cl :::::: li::::::i      C:::::CCCCCCCC::::C  h:::::h     h:::::h a::::a    a:::::a       t::::::tttt:::::t"));
+console.log(chalk.bold.blue("    CC:::::::::::::::Cl :::::: li::::::i       CC:::::::::::::::C  h:::::h     h:::::h a:::::aaaa::::::a       tt::::::::::::::t"));
+console.log(chalk.bold.blue("      CCC::::::::::::Cl :::::: li::::::i         CCC::::::::::::C  h:::::h     h:::::h  a::::::::::aa::: a       tt:::::::::::tt"));
+console.log(chalk.bold.blue("         CCCCCCCCCCCCCl llllll liiiiiiii            CCCCCCCCCCCCC  hhhhhhh     hhhhhhh   aaaaaaaaaa  aaa a         ttttttttttt  "));
+console.log(" ");
+console.log(chalk.bold.bgBlue.black("Made By Aarush Paul"));
+console.log(chalk.yellow("https://github.com/aarush-paul/cli-chat"));
+console.log(" ");
+console.log(" ");
 
 
 
@@ -12,12 +35,15 @@ const rl = readline.createInterface({
   input: process.stdin,
 });
 
-console.log("What is your name?");
+console.log(chalk.bold.bgBlue("What is your name?"));
 rl.question("What is your name?", (text) => {
-    const link = prompt('enter chat url: ');
+    const link = prompt(chalk.bgBlue('enter chat url: '));
+    console.log(" ");
     const socket = io(link);
     socket.emit('new user', text.trim());
-    console.log("You joined the chat");
+    console.log(" ");
+    console.log(chalk.bgGreen("You joined the chat"));
+    console.log(" ");
     process.stdout.write("> ");
     socket.on("message", (text) => {
         process.stdout.write("\r\x1b[K")
--- server.js
+++ server.js
@@ -1,22 +1,47 @@
 const io = require("socket.io")();
-
+const chalk = require("chalk");
 const PORT = process.env.PORT || 3000;
 
 const users = {};
+console.clear();
+                                                                                                             
+console.log(chalk.bold.blue("         CCCCCCCCCCCCCl llllll    iiii              CCCCCCCCCCCCC  hhhhhh                                         tttt          "));
+console.log(chalk.bold.blue("      CCC::::::::::::Cl :::::l   i::::i          CCC::::::::::::C  h::::h                                      ttt:::t          "));
+console.log(chalk.bold.blue("    CC:::::::::::::::Cl :::::l    iiii         CC:::::::::::::::C  h::::h                                      t:::::t          "));
+console.log(chalk.bold.blue("   C:::::CCCCCCCC::::Cl :::::l                C:::::CCCCCCCC::::C  h::::h                                      t:::::t          "));
+console.log(chalk.bold.blue("  C:::::C       CCCCCC  l::::l  iiiiiii      C:::::C       CCCCCC  h::::h hhhhh          aaaaaaaaaaaaa   ttttttt:::::ttttttt    "));
+console.log(chalk.bold.blue(" C:::::C                l::::l  i:::::i     C:::::C                h::::hh:::::hhh       a::::::::::::a  t:::::::::::::::::t    "));
+console.log(chalk.bold.blue(" C:::::C                l::::l   i::::i     C:::::C                h::::::::::::::hh     aaaaaaaaa:::::a t:::::::::::::::::t    "));
+console.log(chalk.bold.blue(" C:::::C                l::::l   i::::i     C:::::C                h:::::::hhh::::::h             a::::a tttttt:::::::tttttt    "));
+console.log(chalk.bold.blue(" C:::::C                l::::l   i::::i     C:::::C                h::::::h   h::::::h     aaaaaaa:::::a       t:::::t          "));
+console.log(chalk.bold.blue(" C:::::C                l::::l   i::::i     C:::::C                h:::::h     h:::::h   aa::::::::::::a       t:::::t          "));
+console.log(chalk.bold.blue(" C:::::C                l::::l   i::::i     C:::::C                h:::::h     h:::::h  a::::aaaa::::::a       t:::::t          "));
+console.log(chalk.bold.blue("  C:::::C       CCCCCC  l::::l   i::::i      C:::::C       CCCCCC  h:::::h     h:::::h a::::a    a:::::a       t:::::t    tttttt"));
+console.log(chalk.bold.blue("   C:::::CCCCCCCC::::Cl :::::: li::::::i      C:::::CCCCCCCC::::C  h:::::h     h:::::h a::::a    a:::::a       t::::::tttt:::::t"));
+console.log(chalk.bold.blue("    CC:::::::::::::::Cl :::::: li::::::i       CC:::::::::::::::C  h:::::h     h:::::h a:::::aaaa::::::a       tt::::::::::::::t"));
+console.log(chalk.bold.blue("      CCC::::::::::::Cl :::::: li::::::i         CCC::::::::::::C  h:::::h     h:::::h  a::::::::::aa::: a       tt:::::::::::tt"));
+console.log(chalk.bold.blue("         CCCCCCCCCCCCCl llllll liiiiiiii            CCCCCCCCCCCCC  hhhhhhh     hhhhhhh   aaaaaaaaaa  aaa a         ttttttttttt  "));
+console.log(" ");
+console.log(chalk.bold.bgBlue.black("Made By Aarush Paul"));
+console.log(chalk.yellow("https://github.com/aarush-paul/cli-chat"));
+                                                                                                                     
+                                                                                                                     
 
 const ngrok = require('ngrok');
 (async function() {
     const url = await ngrok.connect(3000);
-    console.log("Your chat url is:  " + url + ". Send to you friends and ask them to paste this link in the console after running npm run join-a-room command");
+    console.log(chalk.bold.green("Your chat url is:  " + chalk.bold.red(url) + ". Send to you friends and ask them to paste this link in the console after running npm run join-a-room command"));
 })();
 console.log(" ");
-console.log("=====CHAT SERVER LOGS(Press CTRL+Q to destroy room)=====");
+console.log(chalk.bold.bgGreen("=====CHAT SERVER LOGS(Press CTRL+Q to destroy room)====="));
 
     io.on("connection", (socket) => {
-	    console.log("New Connection: " + socket.id);
+	    console.log(chalk.bold.cyan("[!] New Connection: ") + chalk.underline(socket.id));
         socket.on('new user', (name) => {
-            users[socket.id] = name;
-            socket.broadcast.emit("message", `${name} joined the chat.`)
+            users[socket.id] = chalk.bold.yellow(name);
+            socket.broadcast.emit("message", ` `);
+            socket.broadcast.emit("message", chalk.bgYellow(`${name} joined the chat.`));
+            socket.broadcast.emit("message", ` `);
         });
 
         socket.on('message', (text) => {
